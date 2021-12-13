b60bb3e2496a65f8ce0a2f94c8b4fe938684c6aa
Calleum Pecqueux <calleum@posteo.net>
2021-12-13 20:57:00 +1100

    day3

--- src/day3.rs
+++ src/day3.rs
@@ -15,38 +15,36 @@ pub fn solve_1(input: &str) -> u32 {
                 .enumerate()
                 .map(|(i, n)| n + ((bits & 1 << i) >> i))
                 .collect()
-        })
+        }) // returns a vector of count of bit == 1 in each position
         .into_iter()
         .enumerate()
         .map(|(i, b)| ((b >= count / 2) as u32) << i)
         .sum::<u32>();
 
-    let r = ans * (!ans & ((1 << width) - 1));
-    println!("{}", r);
-    r
+    ans * (!ans & ((1 << width) - 1))
 }
 
-// pub fn solve_2(input: &str) -> isize {
-//     let gamma = input
-//         .lines()
-//         .filter_map(|m| m.ok())
-//         .map(|l| usize::from_str_radix(l, 2).unwrap())
-//         .fold(vec![0; 12], |num, bits| {
-//             num.into_iter()
-//                 .enumerate()
-//                 .map(|(i, n)| n + ((bits & 1 << i) >> i))
-//                 .collect()
-//         })
-//         .into_iter()
-//         .enumerate()
-//         .map(|(i, b)| ((b >= count / 2) as u32) << i)
-//         .sum::<u32>();
-// }
+pub fn solve_2(input: &str) -> u32 {
+    let count = input.lines().count();
+    dbg!(count);
+    let width = input.lines().next().unwrap().split("").count() - 2;
+    dbg!(width);
+    let ans = input
+        .lines()
+        .map(|l| usize::from_str_radix(l, 2).unwrap())
+        .fold(vec![0; width], |num, bits| {
+            num.into_iter()
+                .enumerate()
+                .map(|(i, n)| n + ((bits & 1 << i) >> i))
+                .collect()
+        }) // returns a vector of count of bit == 1 in each position
+        .into_iter()
+        .enumerate()
+        .map(|(i, b)| ((b >= count / 2) as u32) << i)
+        .sum::<u32>();
 
-// fn main() {
-//     println!("Solution 1: {:?}", solve_1(&read_input1()));
-//     println!("Solution 2: {:?}", solve_2(&read_input2()));
-// }
+    ans * (!ans & ((1 << width) - 1))
+}
 
 #[cfg(test)]
 mod test {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod day3;
 
 fn main() {
-    day3::solve_1(&day3::read_input());
-    // day3::read_input2()
+    println!("Problem 1: {}", day3::solve_1(&day3::read_input()));
+    //println!("Problem 2: {}", day3::solve_2(&day3::read_input()));
+    day3::solve_2(&day3::read_input());
 }
